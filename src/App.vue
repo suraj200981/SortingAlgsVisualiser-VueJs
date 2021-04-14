bc3e6e148e53f0003747dd9dd80f123ad6312649
ss2261 <ss2261@kent.ac.uk>
2021-04-14 12:22:21 +0100

    Top navigation implemented with mdi icons. Now going to get the page changes done.

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,52 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+  <v-app class="overflow-y-auto">
+    <v-app-bar absolute color="green" dark fixed>
+      <v-toolbar-title>VueJS AlgVisuliser App </v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon large>mdi-chart-bubble</v-icon>
+          </v-btn>
+        </template>
+        <span>Bubble sort algorithm</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+      <v-btn icon v-bind="attrs" v-on="on">
+        <v-icon large>mdi-arrow-down-bold-outline</v-icon>
       </v-btn>
+       </template>
+      <span>Insertion sort algorithm</span>
+      </v-tooltip>
+       <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+      <v-btn icon v-bind="attrs" v-on="on">
+        <v-icon large>mdi-run-fast</v-icon>
+      </v-btn>
+       </template>
+      <span>Quick sort algorithm</span>
+      </v-tooltip>
     </v-app-bar>
 
+    <br />
+    <br />
+    <br />
+    <br /><br />
     <v-main>
-      <HelloWorld/>
+      <!-- <HelloWorld />-->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+//import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 
   data: () => ({
