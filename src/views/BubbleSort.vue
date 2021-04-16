0d996cee040e1b6d181bc3b9a1c46461ec7a7bbb
ss2261 <ss2261@kent.ac.uk>
2021-04-16 20:45:15 +0100

    styling fixed for bubblesort v-text-field and v-btn

--- src/views/BubbleSort.vue
+++ src/views/BubbleSort.vue
@@ -20,16 +20,38 @@
     <br />
     <br />
     <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
     <center>
-      <v-text-field
-        v-model="message3"
-        filled
-        rounded
-        dense
-        prepend-inner-icon="mdi-numeric"
-        label="Enter numbers to sort here"
-        clearable
-      ></v-text-field>
+      <div id="inputContainer">
+        <v-layout row>
+          <v-flex>
+            <v-text-field
+              v-model="message3"
+              filled
+              rounded
+              dense
+              prepend-inner-icon="mdi-numeric"
+              label="Enter numbers to sort here"
+              clearable
+            ></v-text-field>
+          </v-flex>
+          <v-flex>
+            <v-btn
+              :loading="loading3"
+              :disabled="loading3"
+              color="blue-grey"
+              class="ma-2 white--text"
+              @click="loader = 'loading3'"
+            >
+              Submit
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </div>
     </center>
   </div>
 </template>
@@ -46,5 +68,14 @@ export default {
 <style scoped>
 .v-text-field {
   width: 400px;
+}
+.flex,
+.child-flex > * {
+  flex: 1 1 auto;
+  max-width: 40% !important;
+  margin-right: -200px;
+}
+#inputContainer {
+  margin-left: 500px;
 }
 </style>
