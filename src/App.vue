191705a4ca8704ac831800cb417d1e49fb342eb8
ss2261 <ss2261@kent.ac.uk>
2021-04-16 10:50:51 +0100

    vue-routering added - need to put router-links in app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app class="overflow-y-auto">
+    <v-app>
     <v-app-bar absolute color="green" dark fixed>
       <v-toolbar-title>VueJS AlgVisuliser App </v-toolbar-title>
       <v-spacer></v-spacer>
@@ -13,47 +13,47 @@
       </v-tooltip>
       <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
-      <v-btn icon v-bind="attrs" v-on="on">
-        <v-icon large>mdi-arrow-down-bold-outline</v-icon>
-      </v-btn>
-       </template>
-      <span>Insertion sort algorithm</span>
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon large>mdi-arrow-down-bold-outline</v-icon>
+          </v-btn>
+        </template>
+        <span>Insertion sort algorithm</span>
       </v-tooltip>
-       <v-tooltip bottom>
+      <v-tooltip bottom>
         <template v-slot:activator="{ on, attrs }">
-      <v-btn icon v-bind="attrs" v-on="on">
-        <v-icon large>mdi-run-fast</v-icon>
-      </v-btn>
-       </template>
-      <span>Quick sort algorithm</span>
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon large>mdi-run-fast</v-icon>
+          </v-btn>
+        </template>
+        <span>Quick sort algorithm</span>
       </v-tooltip>
     </v-app-bar>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
 
     <br />
     <br />
     <br />
-    <br /><br />
-    <v-main>
-      <br>
-      <br>
-      <Home />
-     
-    </v-main>
-  </v-app>
+    <router-view />
+    <br />
+    <br />
+    </v-app>
 </template>
 
-<script>
-import Home from "./components/Home";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
 
-export default {
-  name: "App",
-
-  components: {
-     Home,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
       <v-hover>
         <v-avatar size="250">
           <img
+          class="bio"
             src="https://media-exp1.licdn.com/dms/image/C5603AQFsJOM49ghWyA/profile-displayphoto-shrink_400_400/0/1581612214075?e=1623888000&v=beta&t=gnpnUmI-qQN3RaN_ttoQnGlr5ocsOdHDtgZtIowE4zQ"
             alt="Suraj Sharma"
           />
